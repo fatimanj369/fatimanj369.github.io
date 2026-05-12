4376cbc59b5ebdaf16f0f167152192c741481ed8
fatimanj369 <f.anjum@my.ccsu.edu>
2026-05-12 14:47:42 -0400

    Add files via upload

---
+++ ch10/lab1001/music/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+	Name: Fatima Anjum
+	File name: script.js
+	May 7, 2026
+*/
+
+/* Hamburger menu function */
+function hamburger () {
+	var menu = document.getElementById("menu-links");
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	} else {
+		menu.style.display = "block";
+	}
+}
